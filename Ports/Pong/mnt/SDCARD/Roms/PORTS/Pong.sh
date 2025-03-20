0dc25986bae8973514ad959cfa45a9bed63ee52d
cobaltgit <cobaltmail4204@gmail.com>
2025-03-20 20:09:50 +0000

    Add Pong clone gluon

---
+++ Ports/Pong/mnt/SDCARD/Roms/PORTS/Pong.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PORT_HOME="$(dirname "$0")/.data/Gong"
+RETROARCH_DIR="/mnt/SDCARD/RetroArch"
+RETROARCH_CORE="$PORT_HOME/gong_libretro.so"
+
+cd "$RETROARCH_DIR"
+
+cp -f "retroarch.cfg" "retroarch_sdl.cfg"
+
+HOME="$RETROARCH_DIR" "$RETROARCH_DIR/ra32.trimui_sdl" -v -L "$RETROARCH_CORE"
+
+cp -f "retroarch_sdl.cfg" "retroarch.cfg"
