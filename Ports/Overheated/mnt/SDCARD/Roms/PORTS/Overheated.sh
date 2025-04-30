afb67223ba6882d2a88325a04e467bdb15ac1c1c
cobaltgit <cobaltsecuremail312@proton.me>
2025-04-30 18:19:46 +0100

    Add select+start exit to overheated

--- Ports/Overheated/mnt/SDCARD/Roms/PORTS/Overheated.sh
+++ Ports/Overheated/mnt/SDCARD/Roms/PORTS/Overheated.sh
@@ -7,4 +7,16 @@ export SDL_VIDEO_FBCON_ROTATION="CCW"
 
 cd "$PORT_HOME"
 
+while true; do
+    ./evtest --query /dev/input/event0 EV_KEY 97
+    if [ $? = 10 ]; then
+        ./evtest --query /dev/input/event0 EV_KEY 28
+        if [ $? = 10 ]; then
+            killall -9 game.elf
+            break
+        fi
+    fi
+    sleep 0.1
+done &
+
 ./game.elf
