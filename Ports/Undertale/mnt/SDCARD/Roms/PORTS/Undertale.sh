e742fbd4e844c44533c24247d182f41c72163d97
cobaltgit <cobaltsecuremail312@proton.me>
2026-05-29 18:43:53 +0100

    ports/Undertale: update butterscotch, allow vanilla exit

--- Ports/Undertale/mnt/SDCARD/Roms/PORTS/Undertale.sh
+++ Ports/Undertale/mnt/SDCARD/Roms/PORTS/Undertale.sh
@@ -10,6 +10,10 @@ PORT_HOME="$(dirname "$0")/.data/$(basename "$0" .sh)"
 export HOME="$PORT_HOME"
 export SDL_VIDEO_FBCON_ROTATION="CCW" # correct rotation for screen (240x320 -> 320x240) for SDL 1.2-based applications
 
+# fix esc not registering, needed to gracefully quit
+L2R2_COMBO=$(cat /sys/module/gpio_keys_polled/parameters/l2r2combokey)
+echo 0 > /sys/module/gpio_keys_polled/parameters/l2r2combokey
+
 cd "$PORT_HOME"
 
 # do stuff here before launching the port
@@ -17,3 +21,5 @@ cd "$PORT_HOME"
 [ -f "gamedata/data.win" ] || exit 1
 
 ./$PORT_EXE $PORT_CMDLINE
+
+echo $L2R2_COMBO > /sys/module/gpio_keys_polled/parameters/l2r2combokey
